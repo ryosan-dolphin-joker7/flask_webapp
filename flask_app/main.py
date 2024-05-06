c6f10f30a8bcce84ff8ff7e1cff98ea857b80187
ryosan-7 <30883561+ryoakira12345@users.noreply.github.com>
2024-05-06 13:53:13 +0900

    テーブルデータの表示を追加しました

--- flask_app/main.py
+++ flask_app/main.py
@@ -3,6 +3,12 @@ from flask import render_template
 
 @app.route('/')
 def index():
-    return render_template(
-        'index.html'
-    )
+    books = [
+        {'title': 'The Great Gatsby',
+        'author': 'F. Scott Fitzgerald',
+        'year': 1925},
+        {'title': 'M',
+        'author': 'Fritz Lang',
+        'year': 1931}
+    ]
+    return render_template('index.html', books=books)
